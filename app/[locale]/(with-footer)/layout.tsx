d4aa503ff41bf73df37b7c2133d695796827f466
Olivia <z846204062@hotmail.com>
2024-07-25 00:48:45 +0800

    Update layout.tsx

--- app/[locale]/(with-footer)/layout.tsx
+++ app/[locale]/(with-footer)/layout.tsx
@@ -1,10 +1,12 @@
 import Footer from '@/components/home/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className='mx-auto flex w-full flex-1'>{children}</main>
       <Footer />
+      <Analytics/>
     </>
   );
 }
